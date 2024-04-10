f47af6293020d5b8d24ed6ca0b6947c6a0ee1446
Eric Chang <fsc8504@gmail.com>
2024-04-10 12:46:53 -0700

    Init setup

---
+++ tax-prep-system/src/main/java/com/jk/taxprep/taxprepsystem/TaxPrepSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jk.taxprep.taxprepsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxPrepSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxPrepSystemApplication.class, args);
+	}
+
+}
---
+++ tax-prep-system/src/test/java/com/jk/taxprep/taxprepsystem/TaxPrepSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.taxprep.taxprepsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxPrepSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
