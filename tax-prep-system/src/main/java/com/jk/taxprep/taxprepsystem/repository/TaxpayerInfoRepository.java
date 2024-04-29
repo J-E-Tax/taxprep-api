e10305f522a2d1cd664639f7f041965dd121fc0a
Eric Chang <fsc8504@gmail.com>
2024-04-28 21:16:27 -0700

    Edit tax payer and user service

--- tax-prep-system/src/main/java/com/jk/taxprep/taxprepsystem/repository/TaxpayerInfoRepository.java
+++ tax-prep-system/src/main/java/com/jk/taxprep/taxprepsystem/repository/TaxpayerInfoRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.jk.taxprep.taxprepsystem.model.TaxpayerInfo;
 
 public interface TaxpayerInfoRepository extends JpaRepository<TaxpayerInfo, Long> {
+    TaxpayerInfo findByUser_UserId(Long userId);
 
 }
--- tax-prep-system/src/main/java/com/jk/taxprep/taxprepsystem/repository/UserRepository.java
+++ tax-prep-system/src/main/java/com/jk/taxprep/taxprepsystem/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.jk.taxprep.taxprepsystem.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jk.taxprep.taxprepsystem.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public Optional<User> findByEmail(String email);
 
 }
